5188b4a39cd330c4e18d93d5bbff0259fa95ede6
Ricohaviqi <ricohaviqi00@gmail.com>
2022-10-06 00:41:45 +0700

    update part3 bagian 3

--- app/src/main/java/com/example/actifitystaff/CrudView.kt
+++ app/src/main/java/com/example/actifitystaff/CrudView.kt
@@ -1,4 +1,18 @@
 package com.example.actifitystaff
 
-interface CrudView {
+interface CrudView {//get data
+fun onSuccessGet(data: List<DataItem>)
+    fun onFailedGet(msg: String)
+
+    //untuk add
+    fun onSuccessAdd(msg: String)
+    fun onErrorAdd(msg: String)
+
+    //untuk update
+    fun onSuccessUpdate(msg: String)
+    fun onErrorUpdate(msg: String)
+
+    //untuk delete
+    fun onSuccessDelete(msg: String)
+    fun onErrorDelete(msg: String)
 }
---
+++ app/src/main/java/com/example/actifitystaff/Presenter2.kt
@@ -0,0 +1,48 @@
+package com.example.actifitystaff
+
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class Presenter2(val crudView: UpdateAddActivity) {
+    //Add data
+    fun aadData(name: String, hp: String, alamat String) {
+        NetworkConfig.getService()
+            .addStaff(name, hp, alamat)
+            .enqueue(object : retrofit2.Callback<ResultStatus> {
+                override fun onFailure(call: Call<ResultStatus>, t: Throwable) {
+                    crudView.onErrorAdd(t.localizedMessage)
+                }
+
+                override fun onResponse(call: Call<ResultStatus>, response: Response<ResultStatus>
+                ) {
+                    if (response.isSuccessful && response.body()?.status == 200) {
+                        crudView.successAdd(response.body()?.pesan ?: "")
+                    } else {
+                        crudView.errorAdd(response.body()?.pesan ?: "" )
+                    }
+                }
+            })
+    }
+}
+    //update data
+    fun upDateData(id: String, name: String, hp: String, alamat: String) {
+        NetworkConfig.getService()
+            .updateStaff(id, name, hp, alamat)
+            .enqueue(object : retrofit2.Callback<ResultStatus> {
+                override fun onFailure(call: Call<ResultStatus>, t: Throwable) {
+                    CrudView.onErrorUpdate(t.localizedMessage)
+                }
+
+                override fun onResponse(call: Call<ResultStatus>, response: Response<ResultStatus>
+                ) {
+                    if (response.isSuccessful && response.body()?.status == 200) {
+                        crudView.onSuccessUpdate(response.body()?.pesan ?: "")
+                    }else {
+                        crudView.onErrorUpdate(response.body()?.pesan ?: "")
+                    }
+                }
+            })
+    }
+}
